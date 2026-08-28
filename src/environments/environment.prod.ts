cc87c4e72dcb019499070ebdcb50df2a99b482c1
Joisner <69428995+Joisner@users.noreply.github.com>
2026-08-28 23:38:10 +0200

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Sustituir por la URL real de la API en producción antes del despliegue.
-  apiUrl: 'https://api.tu-dominio.com/api/v1',
+  apiUrl: 'https://backend-production-83dd.up.railway.app/api/v1',
 };
